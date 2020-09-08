4576fcb1863404682710c6c1d0d0340aa75723f6
caribou <carine@w3.org>
2020-09-08 10:39:40 +0200

    try/catch around call

--- app.js
+++ app.js
@@ -2,28 +2,33 @@ const express = require("express");
 const attendant = require ("./flight-attendant");
 const bodyParser = require("body-parser");
 const { nextTick } = require("process");
-//const { read } = require("fs");
 
 const app = express();
-const PORT = 3000;
-const WEBHOOK_URL = 'https://www.w3.org/users/31823/webhooks/237';
-
+const env = require('dotenv').config({path: __dirname + '/.env'});
+const PORT = process.env['PORT'] || 3000;
+const WEBHOOK_URL = process.env['WEBHOOK_URL'];
 
 app.use(bodyParser.json());
+
 var jsonParser = bodyParser.json();
-app.post("/onboard", jsonParser, (req, res) => {
-    console.log(req.headers);
+app.post("/onboard", jsonParser, (req, res, next) => {
+    if (process.env.NODE_ENV == 'debug'){
+      console.log(req.headers);
+    }
     if (req.header('x-w3c-webhook') == WEBHOOK_URL){
-      var userid = req.body.user.id;
-      var groupid = req.body.group.id;
-      attendant.welcome(userid, groupid);
-      res.status(200).end();
+      try {
+        attendant.welcome(req.body.user.id, req.body.group.id);
+        res.status(200).end();
+      }
+      catch (err){
+        res.status(400).end();
+      }  
     } else {
       console.log('unexpected or forbidden post from '+  (req.headers['x-forwarded-for'] || req.connection.remoteAddress));
       res.status(403).end();
     }
+    next;
   });
-
 
   app.listen(PORT, () => {
     console.log("Server listening on port %d in %s mode", PORT, process.env.NODE_ENV);
